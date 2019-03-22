5bdd34cdd89e7abf87cb278ce76a2a1f284600f6
Thibaut Vitse <thibaut.vitse@easyflirt.com>
2019-03-22 16:41:22 +0100

    fix phpcs

--- phpcs.sh
+++ phpcs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-phpcs -d memory_limit=-1 --standard=ruleset.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity
-phpcs -d memory_limit=-1 --standard=ruleset.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity --cache --report=json | grep -q '^{"totals":{"errors":0,'
+phpcs -d memory_limit=-1 --standard=phpcs.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity
+phpcs -d memory_limit=-1 --standard=phpcs.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity --cache --report=json | grep -q '^{"totals":{"errors":0,'
