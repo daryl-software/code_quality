343e8c649c1b471e4dbad7332dc3f799f0bed1a0
Florian Letanoux <florian.letanoux@easyflirt.com>
2019-04-09 11:57:00 +0200

    Fixed syntax

--- PHPStan/autoload.php
+++ PHPStan/autoload.php
@@ -3,8 +3,5 @@
 require_once __DIR__ . '/EntityManagerDynamicReturnTypeExtension.php';
 
 if (!function_exists('ezGetEnvConfig')) {
-    function ezGetEnvConfig()
-    {
-
-    }
+	function ezGetEnvConfig() {}
 }
