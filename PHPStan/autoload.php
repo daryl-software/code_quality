cf372f7f2d0424d141cf410230e82a0b79bf7d9a
Florian Letanoux <florian.letanoux@easyflirt.com>
2019-04-09 12:02:45 +0200

    Fix the fix of syntax

--- PHPStan/autoload.php
+++ PHPStan/autoload.php
@@ -3,5 +3,7 @@
 require_once __DIR__ . '/EntityManagerDynamicReturnTypeExtension.php';
 
 if (!function_exists('ezGetEnvConfig')) {
-	function ezGetEnvConfig() {}
+    function ezGetEnvConfig()
+    {
+    }
 }
