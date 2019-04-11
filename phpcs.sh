75887ba9fa24aef73a53066be589cc1b6bc55f79
Florian Morello <arsonik@me.com>
2019-04-11 16:26:43 +0200

    test

--- phpcs.sh
+++ phpcs.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-phpcs -d memory_limit=-1 --standard=phpcs.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity
-phpcs -d memory_limit=-1 --standard=phpcs.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity --cache --report=json | grep -q '^{"totals":{"errors":0,'
+./vendor/bin/phpcs -d memory_limit=-1 --standard=phpcs.xml --encoding=utf-8 --extensions=php --runtime-set ignore_warnings_on_exit 1 . --exclude=Generic.Metrics.CyclomaticComplexity --cache --report=full | grep --quiet 'ERROR |'
