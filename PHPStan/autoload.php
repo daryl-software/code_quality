466caf8dca6be0c3561bc329a2e62ce6d714a3f0
Florian Morello <arsonik@me.com>
2019-05-06 09:37:32 +0200

    add getArchiContainer

--- PHPStan/autoload.php
+++ PHPStan/autoload.php
@@ -3,9 +3,14 @@
 require_once __DIR__ . '/EntityManagerDynamicReturnTypeExtension.php';
 
 if (!function_exists('ezGetEnvConfig')) {
-
     function ezGetEnvConfig()
     {
     }
+}
 
+if (!function_exists('getArchiContainer')) {
+
+    function getArchiContainer()
+    {
+    }
 }
